b8a7be56ea852cc6ab9e148ffd9aceeb01c33fd7
Boaz <boazjohn@users.noreply.github.com>
2014-09-19 22:52:09 +0530

    Update pylog.py
    Added input and output arguments.

--- pylog.py
+++ pylog.py
@@ -1,11 +1,18 @@
 from pyspark import SparkContext, SparkConf
 import sys
+import math
+
+def writeToFile(fname, string):
+	f = open(fname,'a')
+	f.write(string)
+	f.close()
+
 
 def Main():
 
 	sc = SparkContext(appName='pylog')
 
-	if len(sys.argv) > 1:
+	if len(sys.argv) == 3:
 		log = sc.textFile(sys.argv[1])
 		log_ne = log.filter(lambda x: len(x) > 0 )
 		line = log_ne.map(lambda x: x.split())
@@ -23,22 +30,30 @@ def Main():
 		tp99 = line.map(lambda x: (int(x[8]),1))\
 			   .sortByKey(lambda x: x)
 
-		index = int(tp99.count()*.99)
+		index = int(math.ceil(tp99.count()*.99))
 		tp = tp99.take(index)
 
+		outlog = 'IP : Count\n'
 		# requests from an IP
 		for (i, icount) in ip.collect():
-			print(i + ' => ' + str(icount))
+			#print(i + ' => ' + str(icount))
+			outlog += i + ' ' + str(icount) + '\n'
+		writeToFile(sys.argv[2]+'/ip_addr.log',outlog)
 
+		outlog = 'Request interval: Count\n'
 		# requests every 10 minute
 		for (ts, tscount) in req_10.collect():
-			print(ts + ' => ' + str(tscount))
+			#print(ts + ' => ' + str(tscount))
+			outlog += ts + ' ' + str(tscount) + '\n'
+		writeToFile(sys.argv[2]+'/req_int.log',outlog)
 
 		# TP99
-		print ('TP99 = ' + str(tp[-1][0]))
+		#print ('TP99 = ' + str(tp[-1][0]))
+		outlog = 'TP99 ' + str(tp[-1][0]) + '\n'
+		writeToFile(sys.argv[2]+'/tp_99.log',outlog)
 
 	else:
-		print 'Usage: pylog <filename>'
+		print 'Usage: pylog <input file> <output path>'
 	
 
 	sc.stop()
